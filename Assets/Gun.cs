3b2a3cb3215bdca0d6d37a505e9ef4c2b4912ae8
maranarasauce <41238916+maranarasauce@users.noreply.github.com>
2021-07-30 22:35:42 -0400

    Gun Animation

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public AudioSource src;
-    public AudioClip fireClip;
-    public AudioClip reloadClip;
+    public AudioSource reloadSrc;
+    public Animator animator;
+    public AnimEvent reloadEvent;
+
+    enum Animation
+    {
+        Idle = 0,
+        Fire = 1,
+        Reload = 2
+    }
 
     Transform camera;
     private void Start()
     {
         camera = Camera.main.transform;
         ammo = maxAmmo;
+        reloadEvent.executedAction += FinishReload;
     }
 
     private void Update()
@@ -24,14 +33,12 @@ public class Gun : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             timeTillFire = 0;
+            if (!reloading)
+                SetAnim(Animation.Idle);
         }
         if (Input.GetMouseButtonDown(1))
         {
-            ammo = maxAmmo;
-            reloading = false;
-            src.clip = reloadClip;
-            src.Play();
-            src.clip = fireClip;
+            
         }
     }
 
@@ -54,15 +61,30 @@ public class Gun : MonoBehaviour
             Reload();
             return;
         }
+        SetAnim(Animation.Fire);
         ammo--;
         src.Stop();
-        src.pitch = UnityEngine.Random.Range(0.99f, 1.01f);
-        src.volume = UnityEngine.Random.Range(0.99f, 1.01f);
+        src.pitch = UnityEngine.Random.Range(0.95f, 1.05f);
+        src.volume = UnityEngine.Random.Range(0.93f, 1.01f);
         src.Play();
     }
 
     void Reload()
     {
+        SetAnim(Animation.Reload);
         reloading = true;
+        reloadSrc.Play();
+    }
+
+    void FinishReload()
+    {
+        SetAnim(Animation.Idle);
+        ammo = maxAmmo;
+        reloading = false;
+    }
+
+    void SetAnim(Animation state)
+    {
+        animator.SetInteger("State", (int)state);
     }
 }
