d63ae46745994183efec8bf900f1fb5c5b178066
Chromium <62724067+thedevi@users.noreply.github.com>
2021-07-31 06:52:07 +0100

    Started Damage Popups

---
+++ Assets/DamagePopUpManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePopUpManager : MonoBehaviour
+{
+    public static DamagePopUpManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    [SerializeField]
+    private GameObject damagePopUpPrefab;
+    public GameObject PopUpParent;
+
+    public void DisplayDamagePopUp(int amount, Transform PopUpParent)
+    {
+        GameObject GO = Instantiate(damagePopUpPrefab, PopUpParent.transform.position, Quaternion.identity, PopUpParent);
+        GO.GetComponent<DamagePopup>().SetUp(amount);
+    }
+}
---
+++ Assets/DamagePopup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamagePopup : MonoBehaviour
+{
+    private Text Text;
+    private Color color;
+    private Transform PlayerTransform;
+
+
+    public float dissapearTimer = 0.5f;
+    public float fadeOutSpeed = 5f;
+    public float moveYSpeed = 1f;
+
+
+    public void SetUp(int amount)
+    {
+        Text = gameObject.GetComponent<Text>();
+        PlayerTransform = Camera.main.transform;
+        color = Text.color;
+        Text.text = amount.ToString();
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(2 * transform.position - PlayerTransform.position);
+        transform.position += new Vector3(0f, moveYSpeed * Time.deltaTime, 0f);
+
+        dissapearTimer = Time.deltaTime;
+        if(dissapearTimer <= 0f)
+        {
+            color.a -= fadeOutSpeed * Time.deltaTime;
+            Text.color = color;
+            if(Text.color.a <= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
