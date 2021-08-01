c4423a07dad78a17e85b27ac9aa075003b86e75c
Parzival-3141 <29632054+Parzival-3141@users.noreply.github.com>
2021-07-31 23:53:23 -0400

    title scene setup and animated skybox

---
+++ Assets/Scripts/Environment/SkyboxController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxController : MonoBehaviour
+{
+    public int rotationSpeed = 5;
+
+    private Material skybox;
+
+    private void Start()
+    {
+        skybox = RenderSettings.skybox;
+    }
+
+    private void Update()
+    {
+        var currentRot = skybox.GetFloat("_Rotation");
+
+        currentRot = Mathf.Repeat(currentRot + rotationSpeed * Time.deltaTime, 360);
+
+        skybox.SetFloat("_Rotation", currentRot);
+    }
+}
